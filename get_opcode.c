12a7a6358a9f3f1cb248acea2de10f3a1bc375a8
djso89 <danielso89@gmail.com>
2020-05-13 18:48:27 -0700

    helper functions for opcodes

---
+++ get_opcode.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * get_opcode - function that selects the operation
+ * @s: string to compare for opcode
+ * Return: Nothing
+ */
+void (*get_opcode(char *s))(stack_t **, unsigned int)
+{
+	instruction_t ops[] = {
+		{"push", add_dstack_list},
+		{"pall", pall_dstack_list},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while(ops[i].opcode != NULL)
+	{
+		if (strcmp(s, ops[i].opcode) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	fprintf(stderr, "L<line_number>: unknown instruction %s\n", s);
+	exit(EXIT_FAILURE);
+}
