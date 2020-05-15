38341f3165a035ec6774337a11bc0d44ac7ab066
bnbasilio <bnbasilio@gmail.com>
2020-05-15 01:12:13 +0000

    added helper functions

--- opcode_helpers.c
+++ opcode_helpers.c
@@ -52,3 +52,71 @@ void pall_dstack_list(stack_t **stack, unsigned int line_number)
 		printf("%d\n", head->n);
 	}
 }
+
+/**
+ * pint_monty - prints the value at the top of the stack
+ * @stack: a starting address of stack doubly linked list
+ * @line_number: line number in file
+ */
+
+void pint_monty(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head = *stack;
+
+	if (head)
+		printf("%d\n", head->n);
+	else
+	{
+		/*FREE*/
+		dprintf(STDERR_FILENO, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * pop_monty - removes the top element of the stack
+ * @stack: a starting address of stack doubly linked list
+ * @line_number: line number in file
+ */
+
+void pop_monty(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	if (!(*stack))
+	{
+		/*FREE*/
+		dprintf(STDERR_FILENO, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	*stack = (*stack)->next;
+	if (*stack)
+		(*stack)->prev = NULL;
+	free(current);
+}
+
+/**
+ * swap_monty - swaps the top two elements of the stack
+ * @stack: a starting address of stack doubly linked list
+ * @line_number: line number in file
+ */
+
+void swap_monty(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (!(*stack) || !(*stack)->next)
+	{
+		/*FREE*/
+		dprintf(STDERR_FILENO, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->next;
+	(*stack)->prev = temp;
+	(*stack)->next = temp->next;
+	temp->prev = NULL;
+	temp->next = *stack;
+	*stack = temp;
+}
