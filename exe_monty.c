5d79379a0dacc782cafc70c4e61c8bf5f53801fe
djso89 <danielso89@gmail.com>
2020-05-16 11:55:18 -0700

    reduced lines in exe_monty function

--- exe_monty.c
+++ exe_monty.c
@@ -1,5 +1,20 @@
 #include "monty.h"
 /**
+ * itr_free - function that iterates the index value and
+ * and free the token string buffer
+ * @l_tok: string token buffer
+ * @i: index value
+ * Return: incremented index
+ */
+unsigned int itr_free(char *l_tok, unsigned int i)
+{
+	unsigned int new = 0;
+
+	new = i + 1;
+	free(l_tok);
+	return (new);
+}
+/**
  * exe_line - function that executes op_fucntions
  * free the string tok buffer and returns next
  * index value
@@ -17,6 +32,21 @@ unsigned int exe_line(char *cmd, char *l_tok, unsigned int i)
 	free(l_tok);
 	return (new);
 }
+/**
+ * get_cmd_arg - fucntion that returns the 2nd argument from
+ * command
+ * @av_line: command line from file
+ * Return: second argument in string
+ */
+char *get_cmd_arg(char *av_line)
+{
+	char *arg = NULL;
+
+	global.l_tok = strdup(av_line);
+	arg = strtok(global.l_tok, " \0");
+	return (arg);
+}
+
 
 /**
  * exe_monty - a fucntion that executes the bytecode .m file
@@ -35,15 +65,13 @@ void exe_monty(char **av_line)
 			i++;
 			continue;
 		}
-		global.l_tok = strdup(av_line[i]);
-		cmd = strtok(global.l_tok, " \0");
+		cmd = get_cmd_arg(av_line[i]);
 		cmd = strtok(NULL, " ");
 		if (cmd != NULL)/*check for another argument after cmd*/
 		{
 			num = strdup(cmd);
 			free(global.l_tok);
-			global.l_tok = strdup(av_line[i]);
-			cmd = strtok(global.l_tok, " ");
+			cmd = get_cmd_arg(av_line[i]);
 			global.value = atoi(num);
 			free(num);
 			if (!(global.value))
@@ -52,12 +80,10 @@ void exe_monty(char **av_line)
 		else
 		{
 			free(global.l_tok);
-			global.l_tok = strdup(av_line[i]);
-			cmd = strtok(global.l_tok, " ");
+			cmd = get_cmd_arg(av_line[i]);
 			if (cmd == NULL || cmd[0] == '#')
 			{
-				i++;
-				free(global.l_tok);
+				i = itr_free(global.l_tok, i);
 				continue;
 			}
 			else if (strcmp(cmd, "push") == 0)
@@ -68,6 +94,7 @@ void exe_monty(char **av_line)
 	}
 	free_stack(global.stack);
 }
+
 /**
  * do_non_int_error - a function that prints error message
  * @l_tok: string token buffer
@@ -76,7 +103,6 @@ void do_non_int_error(char *l_tok)
 {
 	free(l_tok);
 	free_stack(global.stack);
-	free(global.av_line);
-	free(global.line);
+	free_av(global.av_line);
 	print_error_usage(global.line_number);
 }
