edea3febba4bd7bac2d1701316d5096d6f0c4cf3
djso89 <danielso89@gmail.com>
2020-05-16 12:10:15 -0700

    fixed usage error for just monty

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		dprintf(STDERR_FILENO, "Usage: monty file\n");
+		dprintf(STDERR_FILENO, "USAGE:monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
